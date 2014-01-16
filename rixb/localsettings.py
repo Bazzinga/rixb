11de68ac1ba65499bc42879fc721f8e1110473ec
RicterZ <canyuexiaolang@gmail.com>
2014-01-16 18:45:08 +0800

    Create localsettings.py

---
+++ rixb/localsettings.py
@@ -0,0 +1,15 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ''
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'HOST': '',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+    }
+}
